2b6df0575c36cc9b1d06c1e782b766423a90a954
raom <raoming@dcqtech.com>
2018-05-08 15:12:38 +0800

    增加缓存

--- daytask/js/index.js
+++ daytask/js/index.js
@@ -1,70 +1,92 @@
+(function() {
+    var user = location.hash.substr(1);
+    if (!user) user = "rm";
+    window.userid = user;
+    var today = getToday() + "_";
+    var cache = localStorage.getItem("daytask_cache");
+    if (cache) {
+        cache = JSON.parse(cache);
+        if (cache.day == today) {
+            window.tasks = cache.tasks;
+            init();
 
-
-(function () {
-    var user=location.hash.substr(1);
-    if(!user) user="rm";
-    window.userid= user;
-    var today = getToday()+"_";
-    var db = new mlab("tasks","setting");
-    db.find({s:{"权重":-1},q:{"用户":userid}},function(ret){
+        }
+    }
+    var db = new mlab("tasks", "setting");
+    db.find({
+        s: {
+            "权重": -1
+        },
+        q: {
+            "用户": userid
+        }
+    }, function(ret) {
         window.tasks = ret;
+        localStorage.setItem("daytask_cache",JSON.stringify({day:today,tasks:ret}));
         console.info(ret);
-        getDaily();
-        document.getElementById('result').addEventListener("click",function(e){
-            if(e.target.tagName=="LI") {
-                updateTask(e.target);
-                //console.info(e.target.id,e.target.className);
-
-            }
-        },false);
+        init();
     });
 
 })();
 
+function init() {
+    getDaily();
+    document.getElementById('result').addEventListener("click", function(e) {
+        if (e.target.tagName == "LI") {
+            updateTask(e.target);
+        }
+    }, false);
+}
+
 function getToday() {
     var now = new Date();
-    var today = now.getFullYear()*10000+(now.getMonth()+1)*100+now.getDate();
+    var today = now.getFullYear() * 10000 + (now.getMonth() + 1) * 100 + now.getDate();
     return today;
 }
 
 function getDaily() {
-    var db = new mlab("tasks","task");
+    var db = new mlab("tasks", "task");
 
-    db.find({q:{"用户":userid,"日期":getToday()}},function(ret){
+    db.find({
+        q: {
+            "用户": userid,
+            "日期": getToday()
+        }
+    }, function(ret) {
         console.info(ret);
         showResult(ret);
     });
 }
+
 function showResult(items) {
-    var today = getToday()+"_";
-    var lis = window.tasks.map(e=>{
-        return "<li id='"+ today+e["_id"] +"'>"+e["任务"]+"</li>"
+    var today = getToday() + "_";
+    var lis = window.tasks.map(e => {
+        return "<li id='" + today + e["_id"] + "'>" + e["任务"] + "</li>"
     });
     document.getElementById("result").innerHTML = lis.join('');
-    items.forEach(e=>{
-        if(e.ok) {
-            document.getElementById(e._id).className="ok";
+    items.forEach(e => {
+        if (e.ok) {
+            document.getElementById(e._id).className = "ok";
         }
     });
 }
 
 function updateTask(li) {
-    var id=li.id;
-    var ok =  li.className?0:1;
-    var db = new mlab("tasks","task");
-    var item={
-        "_id":id,
-        "ok":ok,
-        "用户":window.userid,
-        "日期":getToday()
+    var id = li.id;
+    var ok = li.className ? 0 : 1;
+    var db = new mlab("tasks", "task");
+    var item = {
+        "_id": id,
+        "ok": ok,
+        "用户": window.userid,
+        "日期": getToday()
     }
-    db.insert(item,function(ret){
-        if(ok) {
+    db.insert(item, function(ret) {
+        if (ok) {
             li.className = "ok";
         } else {
-            li.className="";
+            li.className = "";
         }
         console.info(ret);
     });
 }
-
