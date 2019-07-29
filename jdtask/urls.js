ccadeb849d5f43c05354eb86642d1fa3656057a6
xianghua <xianghua@dcqtech.com>
2019-07-29 13:27:04 +0800

    jd金果

--- jdtask/urls.js
+++ jdtask/urls.js
@@ -41,28 +41,39 @@ var urls = [["https://eimi.cloud/user", "ss 签到", "t0"],
  ["open://com.cmbchina.ccd.pluto.cmbActivity", "APP:掌上生活", "t0"],
  ["open://com.greenpoint.android.mc10086.activity", "APP:中国移动", "t0"],
 
- ["https://bean.m.jd.com/plantBean/index.action?resourceValue=bean", "京豆培养（晚上）", "t2"],
+ ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=473", "京东金融签到", "t2"],
  ["https://coin.jd.com/m/gb/index.html", "领钢镚1", "t2"],
+ ["openapp.jdmobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22couponCenter%22%7D", "京东领劵中心", "t2"],
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=1227", "领红包1227 0.1", "t2"],
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=1272", "领红包1272 0.1", "t2"],
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=1519", "领红包1519 0.1", "t2"],
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=1521", "领红包1521 0.1", "t2"],
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=412", "钢镚 412 0.01", "t2"],
- ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=473", "APP钢镚", "t2"],
+ 
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=815", "领红包815 0.1", "t2"],
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=820", "领券820 0.1", "t2"],
+ ["https://m.jr.jd.com/member/dailyAnswer/", "京东答题", "t2"],
  ["open://com.chinasofti.rcs", "APP:和飞信", "t2"],
  ["open://com.eg.android.AlipayGphone", "APP:支付宝(晚上走路)", "t2"],
  ["open://com.netease.newsreader.activity", "APP:网易新闻", "t2"],
  ["open://com.taobao.etao", "APP:一淘签到", "t2"],
  ["open://com.unionpay", "APP:云闪付", "t2"],
- ["openapp.jdmobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22couponCenter%22%7D", "京东领劵中心", "t2"],
+ 
+ ["https://bean.m.jd.com/plantBean/index.action?resourceValue=bean", "京豆培养（晚上）", "t2"],
+ ["jdmobile://share?jumpType=7&jumpUrl=https://u.jr.jd.com/wxgrowing/moneytree/index.html?channelLV=sq&productId=&channel=pc&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D2339&source=&time=1564377129826", "收金果（12点）", "t2"],
+ ["jdmobile://share?jumpType=7&jumpUrl=https://u.jr.jd.com/wxgrowing/moneytree/index.html?channelLV=sq&productId=&channel=pc&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D2339&source=&time=1564377129826", "收金果（18点）", "t2"],
+ //["https://m.jr.jd.com/spe/downloadApp/index.html?id=2339&jumpType=7&jumpUrl=https%3A%2F%2Fu.jr.jd.com%2Fwxgrowing%2Fmoneytree%2Findex.html%3FchannelLV%3Dsq", "收金果（12点）", "t2"],
+ //["https://m.jr.jd.com/spe/downloadApp/index.html?id=2339&jumpType=7&jumpUrl=https%3A%2F%2Fu.jr.jd.com%2Fwxgrowing%2Fmoneytree%2Findex.html%3FchannelLV%3Dsq", "收金果（18点）", "t2"], 
 
  ["https://bean.m.jd.com/?sid=", "领金豆", "t6"],
  ["jdmobile://share?jumpType=7&channel=default&sourceUrl=1000*https%3A%2F%2Fm.jr.jd.com%2Fspe%2FdownloadApp%2Findex.html%3Fid%3D190&source=&jumpUrl=215", "早起打卡", "t6"],
  ["openapp.jdmobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22IntelligentAssistant%22%7D", "京东小咚", "t6"],
+ 
+
  ["open://com.eg.android.AlipayGphone", "APP:支付宝(早上)", "t6"],
- ["openapp.jdmobile://virtual?params={\"category\":\"jump\",\"des\":\"jdreactcommon\",\"modulename\":\"JDReactJDBeansTaskCenter\",\"appname\":\"JDReactJDBeansTaskCenter\",\"ishidden\":true,\"param\":{\"page\":\"collectJDBeansTaskCenter\",\"transparentenable\":false},\"sourceType\":\"JDReactCollectJDBeans\"}", "进店领豆", "t6"],
+ 
+ //["openapp.jdmobile://virtual?params={\"category\":\"jump\",\"des\":\"jdreactcommon\",\"modulename\":\"JDReactJDBeansTaskCenter\",\"appname\":\"JDReactJDBeansTaskCenter\",\"ishidden\":true,\"param\":{\"page\":\"collectJDBeansTaskCenter\",\"transparentenable\":false},\"sourceType\":\"JDReactCollectJDBeans\"}", "进店领豆", "t6"],
+
  ["https://home.jdpay.com/my/welfare/jxWelfarePage?type=jingdou&source=JDSC", "合集1", "t9"],
  ["https://ljd.m.jd.com/countersign/index.action", "双签奖励", "t9"],
  ["https://m.jr.jd.com/activity/shactivity/payrebate/html/indexhd.html?sid=&cu=true&utm_source=kong&utm_medium=tuiguang&utm_campaign=t_1000550368_&utm_term=9518a0b6fd484b69a9e2df6e6347f05b&abt=3&tab=2", "购物领京豆", "t9"],
