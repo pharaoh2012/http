806c9a527ee8273fdee8048bc36242970bb5b83c
xianghua <xianghua@dcqtech.com>
2019-09-16 11:11:46 +0800

    本地图片

---
+++ cr/images/d.bat
@@ -0,0 +1,12 @@
+
+wget http://pub.dwstatic.com/p/crashroyale/1.0.0/img/user_grade_icon_663abd4.png
+wget "https://cdn.statsroyale.com/images/silver-chest.png"
+wget "https://cdn.statsroyale.com/images/wooden-chest.png"
+wget "https://cdn.statsroyale.com/images/gold-chest.png"
+wget "https://cdn.statsroyale.com/images/giant-chest.png"
+wget "https://cdn.statsroyale.com/images/super-magical-chest.png"
+wget "https://cdn.statsroyale.com/images/super-lightning-chest.png"
+wget "https://cdn.statsroyale.com/images/magical-chest.png"
+wget "https://cdn.statsroyale.com/images/legendary-chest.png"
+wget "https://cdn.statsroyale.com/images/epic-chest.png"
+pause
